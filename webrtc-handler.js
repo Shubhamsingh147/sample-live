9a54565efdc28a0158f7790573b2f2376ae820ee
Solidtude <shubhamsingh147@gmail.com>
2026-05-01 18:10:36 +0530

    Implement WebRTCHandler class for video/audio handling

--- webrtc-handler.js
+++ webrtc-handler.js
@@ -1,1 +1,84 @@
-// webrtc-handler.js content goes here
+class WebRTCHandler {
+    constructor() {
+        this.localStream = null;
+        this.peerConnection = null;
+    }
+
+    initializeLocalStream() {
+        navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+            .then(stream => {
+                this.localStream = stream;
+                // Attach local stream to video element
+            })
+            .catch(error => console.error('Error accessing media devices.', error));
+    }
+
+    createPeerConnection() {
+        this.peerConnection = new RTCPeerConnection();
+        this.localStream.getTracks().forEach(track => {
+            this.peerConnection.addTrack(track, this.localStream);
+        });
+    }
+
+    createOffer() {
+        return this.peerConnection.createOffer().then(offer => {
+            return this.peerConnection.setLocalDescription(offer);
+        });
+    }
+
+    createAnswer() {
+        return this.peerConnection.createAnswer().then(answer => {
+            return this.peerConnection.setLocalDescription(answer);
+        });
+    }
+
+    handleOffer(offer) {
+        return this.peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+    }
+
+    handleAnswer(answer) {
+        return this.peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+    }
+
+    addIceCandidate(candidate) {
+        return this.peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+    }
+
+    toggleMicrophone() {
+        const audioTracks = this.localStream.getAudioTracks();
+        audioTracks.forEach(track => {
+            track.enabled = !track.enabled;
+        });
+    }
+
+    toggleRemoteAudio() {
+        const remoteAudioTracks = this.peerConnection.getReceivers().map(receiver => receiver.track);
+        remoteAudioTracks.forEach(track => {
+            track.enabled = !track.enabled;
+        });
+    }
+
+    startStatsMonitoring() {
+        setInterval(() => {
+            this.peerConnection.getStats(null).then(stats => {
+                this.calculateQualityScore(stats);
+            });
+        }, 1000);
+    }
+
+    calculateQualityScore(stats) {
+        // Implement quality score calculation based on stats
+    }
+
+    close() {
+        this.peerConnection.close();
+        this.localStream.getTracks().forEach(track => track.stop());
+    }
+
+    cleanup() {
+        this.peerConnection = null;
+        this.localStream = null;
+    }
+}
+
+export default WebRTCHandler;

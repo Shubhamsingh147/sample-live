0a888cc70d617e0029b3a97d1afb2fc5d6ac821d
Solidtude <shubhamsingh147@gmail.com>
2026-05-01 18:11:27 +0530

    Update signaling-server.js

--- signaling-server.js
+++ signaling-server.js
@@ -1,1 +1,48 @@
-// signaling-server.js content goes here
+class SignalingServer {
+    constructor() {
+        this.peers = {};
+    }
+
+    initialize() {
+        // Initialization logic, e.g., setting up WebSocket server
+    }
+
+    updatePresence(peerId, isOnline) {
+        if (isOnline) {
+            this.peers[peerId] = true;
+        } else {
+            delete this.peers[peerId];
+        }
+    }
+
+    getOnlinePeers() {
+        return Object.keys(this.peers);
+    }
+
+    sendCallRequest(from, to) {
+        // Logic to send call request to the "to" peer
+    }
+
+    sendCallAnswer(to, answer) {
+        // Logic to send call answer to the "to" peer
+    }
+
+    sendIceCandidate(to, candidate) {
+        // Logic to send ICE candidate to the "to" peer
+    }
+
+    handleMessage(message) {
+        // Logic to handle different types of messages
+    }
+
+    startPolling() {
+        setInterval(() => {
+            // Polling logic to check for updates
+        }, 1000);
+    }
+}
+
+// Example usage:
+const signalingServer = new SignalingServer();
+signalingServer.initialize();
+//...
